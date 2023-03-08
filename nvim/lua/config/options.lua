e5af09ee1ab8f742c9328d1fa91c00924119242f
dmitrix11 <20766924+dmitrix11@users.noreply.github.com>
2023-03-08 06:59:30 -0500

    Added tailwindcss-colorizer and a new guifont to Lazyvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -53,4 +53,32 @@ vim.opt.backup = true -- Automatically save a backup file
 vim.opt.backupdir:remove(".") -- keep backups out of current folder
 
 -- Gui options / settings
-vim.opt.guifont = "MesloLGLDZ_Nerd_Font:h16"
+-- vim.opt.guifont = "MesloLGLDZ_Nerd_Font:h16"
+vim.opt.guifont = "Iosevka Nerd Font Mono:h20,Noto Color Emoji:h20"
+
+-- Neovide settings start
+
+vim.g.neovide_refresh_rate = 30
+vim.g.neovide_refresh_rate_idle = 1
+
+-- vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_unfocused_outline_width = 0.05
+vim.g.neovide_cursor_vfx_mode = "railgun"
+-- vim.g.neovide_cursor_antialiasing = false
+
+vim.g.neovide_hide_mouse_when_typing = true
+
+vim.g.neovide_underline_automatic_scaling = true
+vim.g.neovide_scale_factor = 1.0
+-- Increase font size
+vim.keymap.set("n", "<C-=>", function()
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.10
+end, {})
+-- Decrease font size
+vim.keymap.set("n", "<C-->", function()
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.10
+end, {})
+-- Reset font size
+vim.keymap.set("n", "<C-0>", function()
+  vim.g.neovide_scale_factor = 1.0
+end, {})
---
+++ nvim/lua/plugins/tailwind.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opt = {
+      servers = {
+        tailwindcss = {},
+      },
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+}
